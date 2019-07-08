053d6d21ced4f447dcab451a874476e2d839c3c0
jay18898 <you@example.com>
2019-07-08 12:18:15 +0530

    update routes

--- server.js
+++ server.js
@@ -1,1 +1,7 @@
 var express = require("express")
+var path = require("path")
+const app = express()
+
+// view engine setup        
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
