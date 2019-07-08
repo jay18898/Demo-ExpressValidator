390280d8d32532cbebe4a4e8cd14409f0cbbfa57
jay18898 <you@example.com>
2019-07-08 15:50:57 +0530

    okay

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,8 @@
 var bankController = {};
 const { check, validationResult } = require('express-validator');
 
-bankController.index = (req, res) => res.render(`../views/index`)
+bankController.index = (req, res) => res.render(`../views/index`,{ errors: ''})
+
 bankController.validPage = [
     // username must be an email
     check('emailID', "Invalid email address").isEmail(),
@@ -9,15 +10,10 @@ bankController.validPage = [
     check('password', 'Password is atleast five character').isLength({ min: 5 }),
     check('contactNo',"Invalid contact no").isLength({max:10,min:10})
 ];
+
 bankController.submitIndex = (req, res) => {
-
     const errors = validationResult(req);
-    
-
     console.log(errors.array())
-
     return res.render('../views/index', { errors: errors.array().map((value, key) => value.msg)})
-
-
 };
 module.exports = bankController
